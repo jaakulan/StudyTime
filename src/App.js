af47da6a830b1e7f461a8f912189d13a2e61c975
jaakulan <jaakulans@hotmail.com>
2020-12-28 21:27:44 -0500

    added pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Starter from './Pages/Starter'
+import Keywords from './Pages/Keywords'
 
 function App() {
   return (
     <div className="app-routes">
-      <BrowserRouter>  
+      <BrowserRouter>
         <Switch>
           <Route path="/" component={Starter} />
+          <Route path="/keywords" component={Keywords} />
         </Switch>
       </BrowserRouter>
       </div>
---
+++ src/Pages/Keywords/Keywords.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import styles from "./styles.module.css";
+
+
+const Starter = () => {
+
+    document.body.style.backgroundColor = "white";
+
+    return (
+        <div className={styles.starter}>
+            <img src="StudyTime_icon.png" alt="logo" className={styles.logo}></img>
+            <h1 className={styles.header}>StudyTime</h1>
+            <hr className={styles.headerBreak}></hr>
+        </div>
+    )
+}
+
+export default Keywords;
