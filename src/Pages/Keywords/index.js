827c43711dac371368652b062c92a586987b2114
jaakulan <jaakulans@hotmail.com>
2020-12-30 18:41:50 -0500

    tried checklist

--- src/Pages/Keywords/index.js
+++ src/Pages/Keywords/index.js
@@ -7,22 +7,68 @@ const Keywords = () => {
 
     document.body.style.backgroundColor = "white";
 
+    var myNodelist = document.getElementsByTagName("LI");
+    var i;
+    for (i = 0; i < myNodelist.length; i++) {
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      myNodelist[i].appendChild(span);
+    }
+
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+
+    function newElement(){
+      var li = document.createElement("li");
+      var inputValue = document.getElementById("myInput").value;
+      var t = document.createTextNode(inputValue);
+      li.appendChild(t);
+      if (inputValue === '') {
+        alert("You must write something!");
+      } else {
+        document.getElementById("blockedList").appendChild(li);
+      }
+      document.getElementById("myInput").value = "";
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("X");
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
+
+      for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          var div = this.parentElement;
+          div.style.display = "none";
+        }
+      }
+    }
+
     return (
         <div className={styles.starter}>
-          <form action="/">
             <img src="StudyTime_icon.png" alt="logo" className={styles.logo}></img>
             <h1 className={styles.header}>StudyTime</h1>
             <hr className={styles.headerBreak}></hr>
             <img src={keyword_active} alt="logo" className={styles.pagelogo}></img>
-              <div className={styles.queryContainer}>
-                    <label for="name">Any keywords entered here will block websites which contain them. Please enter all keywords you want blocked using comma's or whitespaces to seperate them:</label>
-                    <input type="text" id="textbox" name="keywordsAdded" placeholder="Type the keywords here.."></input>
+            <h2>Any keywords entered here will block websites which contain them. Please enter all keywords you want blocked using comma's or whitespaces to seperate them:</h2>
+              <div id="myDIV">
+                <input type="text" id="myInput" placeholder="Enter words here..."></input>
+                <button onClick={newElement} className={styles.addBtn}>Add this</button>
               </div>
 
+              <ul id="blockedList">
+              </ul>
+
               <div className={styles.submitContainer}>
-               <input type="submit" value="Block these!"></input>
+               <button className={styles.block}>"Block these!"</button>
               </div>
-          </form>
           <button className={styles.back}>Nevermind, Take me Back!</button>
         </div>
     )
